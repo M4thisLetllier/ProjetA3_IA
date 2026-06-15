e0c2f6e7f6c30ff9b0eed2d86604f053d2aa674d
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2026-06-15 22:42:37 +0200

    création du fichier configuration avec les datas importantes du dossier

---
+++ configuration.py
@@ -0,0 +1,15 @@
+import os
+
+# --- CONFIGURATION GLOBALE ---
+DOSSIER_CARTES = "carte"
+DOSSIER_MODELES = "modeles"
+COULEURS = [
+        'red', 'blue', 'green', 'purple', 'orange',
+        'darkred', 'darkblue', 'darkgreen', 'cadetblue',
+         'pink', 'lightblue', 'lightgreen', 'gray',
+         'lightgray', 'gold', 'magenta', 'cyan',
+        'brown', 'lime', 'teal', 'navy', 'olive'
+    ]
+def creation_dossier():
+    os.makedirs(DOSSIER_CARTES, exist_ok=True)
+    os.makedirs(DOSSIER_MODELES, exist_ok=True)
