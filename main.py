558322711b5db0d1058a0fcf1aefb6ea732fbea0
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2026-06-15 22:46:44 +0200

    création d'un salon qui redispatch vers les hubs, le but : interface graphique dans le .exe

---
+++ main.py
@@ -0,0 +1,64 @@
+import sys
+import webbrowser
+import pandas as pd
+from Part2_Clusturing.Kmeans_Hub import KmeansPage
+from Part2_Clusturing.DbScan_Hub import DbscanPage
+from configuration import creation_dossier
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
+                             QSlider, QLabel, QPushButton, QMessageBox, QFrame)
+from PyQt5.QtCore import Qt
+
+
+
+
+class MainHub(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Panel d'Analyse des Bornes de Recharge")
+        self.setFixedSize(400, 300)
+
+        # Simulation chargement données
+        self.charger_donnees()
+
+        layout = QVBoxLayout()
+        title = QLabel("Menu Principal Analysis")
+        title.setStyleSheet("font-size: 18px; font-weight: bold; margin-bottom: 20px;")
+        title.setAlignment(Qt.AlignCenter)
+        layout.addWidget(title)
+
+        self.btn_kmeans = QPushButton("Ouvrir Analyse K-MEANS")
+        self.btn_kmeans.setFixedHeight(50)
+        self.btn_kmeans.clicked.connect(self.open_kmeans)
+        layout.addWidget(self.btn_kmeans)
+
+        self.btn_dbscan = QPushButton("Ouvrir Analyse DBSCAN")
+        self.btn_dbscan.setFixedHeight(50)
+        self.btn_dbscan.clicked.connect(self.open_dbscan)
+        layout.addWidget(self.btn_dbscan)
+
+        footer = QLabel("D'autres analyses seront ajoutées ultérieurement...")
+        footer.setStyleSheet("color: gray; font-size: 10px;")
+        footer.setAlignment(Qt.AlignCenter)
+        layout.addWidget(footer)
+
+        self.setLayout(layout)
+
+    def charger_donnees(self):
+        self.df = pd.read_csv("IRVE_clean_FINAL.csv")
+        self.coords = self.df[['lon', 'lat']].dropna()
+
+    def open_kmeans(self):
+        self.km_win = KmeansPage(self.coords, self.df)
+        self.km_win.show()
+
+    def open_dbscan(self):
+        self.db_win = DbscanPage(self.coords, self.df)
+        self.db_win.show()
+
+
+if __name__ == "__main__":
+    creation_dossier()
+    app = QApplication(sys.argv)
+    hub = MainHub()
+    hub.show()
+    sys.exit(app.exec_())
